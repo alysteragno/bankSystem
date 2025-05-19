19168abfd08818240683c2db9132f4bc450d1e27
Alyster <alysteragno@gmail.com>
2025-05-19 15:54:36 +0800

    feat(main page): created header with logo

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <img src="img\bdo_logo.jpg" alt="">
+    </header>
+
+    <div>
+        <ul>
+            <li><a href="balance.php"></a></li>
+            <li><a href="deposit.php"></a></li>
+            <li><a href="withdraw.php"></a></li>
+        </ul>
+    </div>
+</body>
+</html>
